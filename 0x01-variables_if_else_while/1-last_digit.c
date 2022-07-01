4f49d09b7be6621d730779e6c4d8861e027b155b
Lourdel Kigudde <klourdel@gmail.com>
2022-07-01 14:44:45 -0700

    Kyoka Bettygit add .

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,10 @@
 #include<stdio.h>
 /**
  * Main- determines either number is grater than 5, less than 6 or is 0
+ *
  * Description: print the last digit of the number stored in the variable n
  *
- * Return: always 0 (success)
+ * Return:always 0 (success)
 */
 int main(void)
 
