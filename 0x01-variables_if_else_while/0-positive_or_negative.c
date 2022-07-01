a27741da02cd1a71dc2d2e0d4b58d1af1cc940a7
Lourdel Kigudde <klourdel@gmail.com>
2022-07-01 12:21:52 -0700

    fixxing error with betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,12 @@
 #include<stdlib.h>
 #include<time.h>
 #include<stdio.h>
-
-/** 
- *
+/**
  *main- determines if number is  positive or negative
  *
  *Return: always 0 (success)
  *
- * */
+*/
 int main(void)
 {
 	int n;
@@ -16,16 +14,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{	
+	{
 		printf("%d is positive\n", n);
 	}
-	else if (n==0)
+	else if (n == 0)
 	{
-		printf ("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf ("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
