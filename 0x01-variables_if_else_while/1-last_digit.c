ff7f93a87c33f8bf998e9a05eb3bdc72eba54217
Lourdel Kigudde <klourdel@gmail.com>
2022-07-01 13:56:23 -0700

    another fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,24 +3,28 @@
 #include<stdio.h>
 /*
  * Main determines either number is grater than 5, less than 6 or is 0
+ *
  * Return: always 0 (success)
 */
-int main(void)
+int main(void) /*main does  number comparison */
+
 {
-	int n;
+	int n, l;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 5)
+	l = n % 10;
+	if (l > 5)
 	{
-	printf("Last digit of %d is %d and is grater than 5\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
-	else if (n == 0)
+	else if (l == 0)
 	{
-	printf("Last digit of %d is %d and is grater than 0\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
 	else
 	{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
 	return (0);
 }
