c41d7a86325f600be79aa68538eeec6d2ba61fa1
Lourdel Kigudde <klourdel@gmail.com>
2022-07-04 12:16:31 -0700

    Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,52 +1,32 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - prints numbers btn 00 to 99
- *
- * Return: always 0
+ * main - writes 00 - 99
+ * Decription: Writes all unique combinations
+ * Return: zero for success
 */
 int main(void)
 {
-	int i, e, g, h, op1, op2;
+	int i, j;
 
-	i = e = g = h = 48;
-
-	while (h < 58)
+	for (i = 0; i <= 99; i++)
 	{
-		g = 48;
-		while (g < 58)
+		for (j = 0; j <= 99; j++)
 		{
-			e = 48;
-			while (e < 58)
+			if (i < j)
 			{
-				i = 48;
-				while (i < 58)
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+
+				if (i != 98 || j != 99)
 				{
-					op1 = (h * 10) + g;
-					op2 = (e * 10) + i;
-					if (op1 < op2)
-
-					{
-						putchar(h);
-						putchar(g);
-						putchar(' ');
-						putchar(e);
-						putchar(i);
-
-						if (h == 57 && g == 56 && e == 57 && i == 57)
-						{
-						putchar(',');
-						putchar(' ');
-						break;
-						}
-					}
-					i++;
+					putchar(',');
+					putchar(' ');
 				}
-				e++;
 			}
-			g++;
-
 		}
-		h++;
 	}
 	putchar('\n');
 	return (0);
