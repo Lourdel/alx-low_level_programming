6397980372eab55bdbe4e3de8bf6c10bb114af12
Lourdel Kigudde <klourdel@gmail.com>
2022-07-05 15:36:35 -0700

    alpha fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * main -prints the alphabet in lowercase followed by a new line
+ * print_alphabet -prints the alphabet in lowercase followed by a new line
  * Return: always 0
  *
 */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int i;
 
