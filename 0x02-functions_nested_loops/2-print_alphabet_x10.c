ee800f00ae50ec51fd78061c5dc42b7d41722a68
Lourdel Kigudde <klourdel@gmail.com>
2022-07-05 15:47:35 -0700

    10xalphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10.c - prints the lower case alphabet 10 time
+ * Return: always 0
+ *
+*/
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
+
