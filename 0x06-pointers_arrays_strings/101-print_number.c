4a9529bfed00a51969e52eeea0b9aeb6b0bd3efc
Lourdel Kigudde <klourdel@gmail.com>
2022-07-14 12:44:31 -0700

    fix print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,29 +7,18 @@
  */
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
 		_putchar(45);
-		m = n * -1;
-	}
-	else
-	{
-		m = n;
+		num = -num;
 	}
 
-	d = m;
-	count = 1;
-
-	while (d > 9)
+	if ((num / 10) > 0)
 	{
-		d /= 10;
-		count *= 10;
+		print_number(num / 10);
 	}
 
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+	_putchar((num % 10) + '0');
 }
