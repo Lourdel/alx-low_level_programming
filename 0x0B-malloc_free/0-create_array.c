39521e8031403d67d379c5382686a4114f4c4659
Lourdel Kigudde <klourdel@gmail.com>
2022-07-25 12:01:44 -0700

    Adding Readme, Mani.h and create_array function

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+# include "main.h"
+
+/**
+ * create_array- creates an array of characters
+ * and initialises it with a specific character
+ * @size: size of an array
+ * @c: character stored
+ * Return: pointer of an array of char
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(c) * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+## ALX-Low Level Programming
+	*malloc and free functions
+	*Understanding the functional operation of malloc and free in C
